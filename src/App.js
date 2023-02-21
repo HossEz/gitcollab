8c457d55da414b5e95c1c082ec4d39d7a2e7b003
Adyan <adyan850@gmail.com>
2023-02-21 14:18:42 +0100

    penger

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import './main.css'
 import RecipeCard from './components/RecipeCard';
 function App() {
   return (
-    <>
-    <div id='container'>
-    <header>
-      <h1>Penger</h1>
-    </header>
-    <main>
-    <RecipeCard title="Pizza" ingress="Italiensk Pizza"/>
-    </main>
-    <footer>
-
-    </footer>
-    </div>
-    </>
+    <Main/>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+export default function Main(){
+    return(
+        <div id='container'>
+        <header>
+          <h1>Penger</h1>
+        </header>
+        <main>
+        <RecipeCard title="Pizza" ingress="Italiensk Pizza"/>
+        </main>
+        <footer>
+    
+        </footer>
+        </div>
+    )
+}
