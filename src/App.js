48be5a678f6f74037e1745c65fae9845470db826
HossEz <41141267+HossEz@users.noreply.github.com>
2023-02-23 08:52:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import './recipes.css';
 import Main from './components/Main';
 import {Route, Routes} from 'react-router-dom'
 import RecipePage from './components/RecipePage';
-import recipes from './resources/recipes';
+// import recipes from './resources/recipes';
+import Layout from './components/Layout';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [recipes, setRecipes] = useState([])
+
+  const getRecipes = async() =>{
+    const response = await fetch('https://api.edamam.com/search?q=american&app_id=9c154bcd&app_key=59de9ed7e55180cb89b6550c6c272788')
+    const data = await response.json()
+    console.log(data.hits)
+    setRecipes(data.hits)
+}
+
+useEffect(() => {
+  getRecipes()
+}, [])
+
   return (
-    <Routes>
-      <Route index element={<Main/>} />
-      <Route path=':slug' element={<RecipePage recipes={recipes}/>} />
-    </Routes>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route index element={<Main recipes={recipes}/>} />
+          <Route path=':slug' element={<RecipePage recipes={recipes}/>} />
+        </Route>
+      </Routes>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,15 @@
 import RecipeCard from "./RecipeCard";
-import recipes from "../resources/recipes";
+// import recipes from "../resources/recipes";
 import { Link } from "react-router-dom";
 
-export default function Main(){
+export default function Main({recipes}){
     return (
-        <div id='container'>
-            <header>
-                <h1>Penger</h1>
-            </header>
-            <main>
-                {recipes.map((recipe) => (
-                    <Link className="haha" to={recipe.slug}>
-                        <RecipeCard title={recipe.title}/>
+        <>
+                {recipes.map((recipe, index) => (
+                    <Link className="recipes" key={index} to={recipe?.recipe?.label.replace(/\s/g, "-").toLowerCase()}>
+                        <RecipeCard title={recipe?.recipe?.label}/>
                     </Link>
                 ))}
-            </main>
-            <footer>
-        
-            </footer>
-        </div>
-    )
+                </>
+    );
 }
